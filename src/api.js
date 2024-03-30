049bda239da148feaf34aa7cff9e61383cbef6e1
Slavikkq <yaroslavmanuljak@gmail.com>
2024-03-30 11:49:48 +0200

    react hw 05

--- src/api.js
+++ src/api.js
@@ -1,20 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = '83f18c58a5384b5487e6770f2590d9dd';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = "83f18c58a5384b5487e6770f2590d9dd";
+const BASE_URL = "https://api.themoviedb.org/3";
 
-export default function searchMovies(query) {
-  return axios.get(`${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}`)
-    .then(response => response.data.results)
-    .catch(error => {
-      throw new Error('Error searching movies:', error);
+export function searchMovies(query) {
+  return axios
+    .get(`${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}`)
+    .then((response) => response.data.results)
+    .catch((error) => {
+      throw new Error("Error searching movies:", error);
     });
 }
 
-export default function getMovieDetails(movieId) {
-  return axios.get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`)
-    .then(response => response.data)
-    .catch(error => {
-      throw new Error('Error fetching movie details:', error);
+export function getMovieDetails(movieId) {
+  return axios
+    .get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw new Error("Error fetching movie details:", error);
     });
 }
